d8cd05b6a961d48b942984a67350ea7719bf341a
Padme MC <emanuele.leonardi@roma1.infn.it>
2015-12-11 17:23:52 +0100

    Initial version of ECalDetector class

---
+++ PadmeMC/ECal/include/ECalDetector.hh
@@ -0,0 +1,35 @@
+// ECalDetector.hh
+// --------------------------------------------------------------
+// History:
+//
+// Created by Emanuele Leonardi (emanuele.leonardi@roma1.infn.it) 2105-12-11
+// --------------------------------------------------------------
+
+#ifndef ECalDetector_H
+#define ECalDetector_H 1
+
+#include "globals.hh"
+
+class G4LogicalVolume;
+
+//class ECalDetectorMessenger;
+
+class ECalDetector
+{
+
+public:
+
+  ~ECalDetector();
+  ECalDetector(G4LogicalVolume*);
+
+  void CreateGeometry();
+
+private:
+
+  G4LogicalVolume* fMotherVolume;
+
+  //ECalDetectorMessenger* fECalMessenger;
+
+};
+
+#endif

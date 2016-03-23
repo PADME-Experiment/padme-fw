4d609b5967e97a9b279cac6a03fe1108ca1d4593
Padme MC <emanuele.leonardi@roma1.infn.it>
2016-03-23 14:16:32 +0100

    Define PadmeReconstruction class and associated virtual classes

---
+++ PadmeReco/RecoBase/include/PadmeVReconstruction.hh
@@ -0,0 +1,51 @@
+#ifndef PadmeVReconstruction_H
+#define PadmeVReconstruction_H
+
+#include "TFile.h"
+#include "TH1D.h"
+#include "TH2D.h"
+
+#include "TPadmeEvent.hh"
+
+#include "PadmeVNamedModule.hh"
+
+class PadmeVReconstruction : public PadmeVNamedModule
+{
+public:
+
+  PadmeVReconstruction(TFile*, TString, TString);
+  virtual ~PadmeVReconstruction();
+  //virtual TRecoVEvent* ProcessEvent(TDetectorVEvent* = 0, Event* = 0) = 0;
+  virtual void Init(PadmeVReconstruction*) = 0;
+  virtual void EndProcessing() = 0; ///< Call from derived classes
+  virtual void ParseConfFile(TString);
+
+  static void Exception(TString);
+
+public:
+
+  //TRecoVEvent* GetRecoEvent() { return fRecoEvent; };
+  //void         SetRecoEvent(TRecoVEvent* value) { fRecoEvent = value; };
+
+  PadmeVReconstruction* GetMainReco() { return fMainReco; };
+  void                  SetMainReco(PadmeVReconstruction* value) { fMainReco = value; };
+
+  TFile* GetHistoFile() { return fHistoFile; };
+
+  TString GetConfigFileName() { return fConfigFileName; };
+  void    SetConfigFileName(TString val) { fConfigFileName = val; };
+
+  // Use to get an existing directory or create if not already made
+  //TDirectory* GetOrMakeDir(TDirectory *inDir,TString dirName);	  
+
+protected:
+
+  TFile* fHistoFile;
+  PadmeVReconstruction* fMainReco;
+
+  //TRecoVEvent * fRecoEvent;
+
+  TString   fConfigFileName;
+
+};
+#endif

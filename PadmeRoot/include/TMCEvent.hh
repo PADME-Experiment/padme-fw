6c261f9e150952ad08b2db65e26571ec074d4297
Padme MC <emanuele.leonardi@roma1.infn.it>
2016-05-16 10:27:05 +0200

    Renamed TPadmeEvent to TMCEvent

--- PadmeRoot/include/TMCEvent.hh
+++ PadmeRoot/include/TMCEvent.hh
@@ -4,23 +4,21 @@
 // Created by Emanuele Leonardi (Emanuele.Leonardi@cern.ch) 2016-03-15
 //
 // --------------------------------------------------------------
-#ifndef TPadmeEvent_h
-#define TPadmeEvent_h 1
+#ifndef TMCEvent_h
+#define TMCEvent_h 1
 
 #include "TObject.h"
 #include "TClonesArray.h"
 
-//#include "KinePart.hh"
-//#include "TRandom3.h"
 #include "Riostream.h"
 
-class TPadmeEvent: public TObject
+class TMCEvent: public TObject
 {
 
 public:
 
-  TPadmeEvent();
-  virtual ~TPadmeEvent() { Clear(); };
+  TMCEvent();
+  virtual ~TMCEvent() { Clear(); };
 
   Int_t GetRunNumber() { return fRunNumber; }
   void  SetRunNumber(Int_t val) { fRunNumber = val; }
@@ -34,19 +32,6 @@ public:
   void Clear(Option_t* option ="");
   void Reset(Option_t* option ="");
   void Print(Option_t* option="") const;
-  //void PrintAll() const;
-
-  //Int_t GetNPurgedKineParts() { return fNPurgedKineParts; };
-
-  //Int_t         GetNKineParts() const { return fNKineParts; }
-  //TClonesArray* GetKineParts()  const { return fKineParts; }
-  //KinePart*     AddKinePart();
-  //void          EraseKineParts();
-
-  //Int_t         GetNGeneParts() const { return fNGeneParts; }
-  //TClonesArray* GetGeneParts()  const { return fGeneParts; }
-  //GenePart*     AddGenePart();
-  //void          EraseGeneParts();
 
   //void StoreRandomState(TRandom3* RandomDecayState, long *RanecuState);
   //TRandom3* GetRandomDecayState () {return fRandomDecayState;}
@@ -58,17 +43,10 @@ private:
   Int_t fEventNumber;
   Double_t fTime;
 
-  //Int_t fNGeneParts;        // Number of generated particles
-  //Int_t fNKineParts;        // Number of saved particles
-  //Int_t fNPurgedKineParts;  // Number of removed KineParts
-
-  //TClonesArray* fKineParts; // Array of saved particles
-  //TClonesArray* fGeneParts; // Array of generated particles in K rest frame
-
   //static TRandom3* fgRandomDecayState;
   //TRandom3* fRandomDecayState;
   //Long_t fRanecuState[2];
 
-  ClassDef(TPadmeEvent,1)
+  ClassDef(TMCEvent,1)
 };
 #endif

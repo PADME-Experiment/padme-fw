06386fc39be821079350554eba816a192e0a53e3
eleonardi <emanuele.leonardi@roma1.infn.it>
2016-07-06 15:18:38 +0200

    Added zsh configuration file

---
+++ Configure/padme_init.zsh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Find path to PADME software from position of this script
+# assuming that this script is located in the Configure subdirectory
+# of the main PADME software directory
+export PADME=$0:A:h:h
+
+# Define PADMEMC and Geant4 working dir
+export PADMEMC=${PADME}/PadmeMC
+export G4WORKDIR=${PADMEMC}
+
+# Define PADMERECO
+export PADMERECO=${PADME}/PadmeReco
+
+# Define path to PadmeRoot and add it to LD_LIBRARY_PATH
+export PADMEROOT=${PADME}/PadmeRoot
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${PADMEROOT}/lib"
+
+# Configure PadmeDB access parameters fom convenitonal file
+if [ -f ${HOME}/.padme-dbaccess.sh ]
+then
+    source ${HOME}/.padme-dbaccess.sh
+else
+    echo "WARNING - File ${HOME}/.padme-dbaccess.sh not found: no DB access"
+fi

792d0cda98f4ce14c14b40366f4cd5235c5f42c2
Emanuele Leonardi <emanuele.leonardi@roma1.infn.it>
2017-03-28 18:17:44 +0200

    Enabled ECal cluster reconstruction in PadmeReco

--- PadmeReco/ECal/src/ECalReconstruction.cc
+++ PadmeReco/ECal/src/ECalReconstruction.cc
@@ -8,6 +8,12 @@
 
 #include "ECalReconstruction.hh"
 
+#include "ECalCrystalHandler.hh"
+#include "ECalCrystal.hh"
+#include "ECalClusterFinder.hh"
+#include "ECalClusterFinderBox.hh"
+#include "ECalCluster.hh"
+
 #include "TECalMCEvent.hh"
 #include "TECalMCHit.hh"
 #include "TECalMCDigi.hh"
@@ -70,6 +76,35 @@ void ECalReconstruction::ProcessEvent(TMCVEvent* tEvent, TMCEvent* tMCEvent)
     TECalMCDigi* digi = (TECalMCDigi*)tECalEvent->Digi(iD);
     digi->Print();
   }
+
+  // Let's do some cluster finding
+  ECalCrystalHandler* cryHand = new ECalCrystalHandler();
+  for (Int_t iD=0; iD<tECalEvent->GetNDigi(); iD++) {
+    TECalMCDigi* digi = (TECalMCDigi*)tECalEvent->Digi(iD);
+    Int_t iX = digi->GetChannelId()/100;
+    Int_t iY = digi->GetChannelId()%100;
+    ECalCrystal* cry = cryHand->CreateCrystal(iX,iY);
+    cry->SetEnergy(digi->GetSignal()); // Will need a signal->energy converter
+    cry->Print();
+  }
+  cryHand->SortEnergy();
+
+  // Find clusters with PadmeIsland algorithm
+  ECalClusterHandler* cluHand = new ECalClusterHandler();
+  ECalClusterFinder* cluFind = new ECalClusterFinder(cryHand,cluHand);
+  cluFind->SetEThreshold(0.1); //min thr in each crystal
+  cluFind->SetEThresholdSeed(10.); //min thr for seed crystal
+  Int_t newNClu = cluFind->FindClusters();
+  cluHand->Print();
+
+  // Find clusters with PadmeRadius algorithm
+  ECalClusterHandler* cluHandBox = new ECalClusterHandler();
+  ECalClusterFinderBox* cluFindBox = new ECalClusterFinderBox(cryHand,cluHandBox);
+  cluFindBox->SetEThreshold(0.1); //min thr in each crystal
+  cluFindBox->SetEThresholdSeed(10.); //min thr for seed crystal
+  Int_t newNCluBox = cluFindBox->FindClusters();
+  cluHandBox->Print();
+
 }
 
 void ECalReconstruction::EndProcessing()

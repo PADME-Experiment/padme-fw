e17fac7ddafe4845a2feeed08fee565bbf6d830e
eleonardi <emanuele.leonardi@roma1.infn.it>
2017-10-23 15:10:07 +0200

    Make ADC window fully resizable

--- RunControl/code/ADCBoardGUI.py
+++ RunControl/code/ADCBoardGUI.py
@@ -25,17 +25,21 @@ class ADCBoardGUI:
 
         # Initialize main graphic window
         self.root = Tk()
-        #self.root.geometry("579x485+500+500")
-        self.root.geometry("579x498")
-        self.root.configure(background='black')
+        self.root.geometry("400x600")
+        self.root.configure(background='grey')
         self.root.title(self.b_name)
+        self.root.minsize(200,200)
+
+        # Resizing only stretches the text window, i.e. (0,0) on the grid
         self.root.grid_columnconfigure(0,weight=1)
+        self.root.grid_columnconfigure(1,weight=0)
         self.root.grid_rowconfigure(0,weight=1)
         self.root.grid_rowconfigure(1,weight=0)
+        self.root.grid_rowconfigure(2,weight=0)
 
         # Button to quit board configuration window
         self.b_done = Button(self.root,text="Done",fg="red",command=self.close_gui)
-        self.b_done.grid(row=1,column=0,sticky=W+E)
+        self.b_done.grid(row=2,column=0,sticky=W+E)
 
         if (self.mode=="cfg"): self.show_cfg()
         if (self.mode=="log"): self.show_log()
@@ -55,28 +59,13 @@ class ADCBoardGUI:
 
     def show_cfg(self):
 
-        # Frame to show configuration
-        #self.f_cfg = Frame(self.root,bg="black")
-        #self.f_cfg.grid(row=0,column=0,sticky=NSEW)
-        #self.s_cfg = Scrollbar(self.f_cfg)
-        #self.w_cfg = Text(self.f_cfg,height=30,width=80)
-        #self.s_cfg.pack(side=RIGHT,fill=Y)
-        #self.w_cfg.pack(side=LEFT,fill=Y)
-        #self.s_cfg.config(command=self.w_cfg.yview)
-        #self.w_cfg.config(yscrollcommand=self.s_cfg.set)
-        #self.w_cfg.insert(END,"=== Board configuration ===\n")
-        #self.w_cfg.insert(END,self.config)
+        self.w_cfg = Text(self.root,wrap=NONE)
+        self.w_cfg.grid(row=0,column=0,sticky=NSEW)
 
-        self.f_cfg = Frame(self.root,bg="black")
-        self.f_cfg.grid(row=0,column=0,sticky=NSEW)
-
-        self.w_cfg = Text(self.f_cfg,height=30,width=80,wrap=NONE)
-        self.w_cfg.grid(row=0,column=0)
-
-        self.sx_cfg = Scrollbar(self.f_cfg,orient=HORIZONTAL,command=self.w_cfg.xview)
+        self.sx_cfg = Scrollbar(self.root,orient=HORIZONTAL,command=self.w_cfg.xview)
         self.sx_cfg.grid(row=1,column=0,sticky=EW)
 
-        self.sy_cfg = Scrollbar(self.f_cfg,orient=VERTICAL,command=self.w_cfg.yview)
+        self.sy_cfg = Scrollbar(self.root,orient=VERTICAL,command=self.w_cfg.yview)
         self.sy_cfg.grid(row=0,column=1,sticky=NS)
 
         self.w_cfg.config(xscrollcommand=self.sx_cfg.set)
@@ -86,27 +75,13 @@ class ADCBoardGUI:
 
     def show_log(self):
 
-        # Frame to show log file
-        #self.f_log = Frame(self.root,bg="black")
-        #self.f_log.grid(row=0,column=0,sticky=NSEW)
-        #self.s_log = Scrollbar(self.f_log)
-        #self.w_log = Text(self.f_log,height=30,width=80)
-        #self.s_log.pack(side=RIGHT,fill=Y)
-        #self.w_log.pack(side=LEFT,fill=Y)
-        #self.s_log.config(command=self.w_log.yview)
-        #self.w_log.config(yscrollcommand=self.s_log.set)
-        #self.w_log.insert(END,"=== Board log file "+self.log_file+" ===\n")
+        self.w_log = Text(self.root,wrap=NONE)
+        self.w_log.grid(row=0,column=0,sticky=NSEW)
 
-        self.f_log = Frame(self.root,bg="black")
-        self.f_log.grid(row=0,column=0,sticky=NSEW)
-
-        self.w_log = Text(self.f_log,height=30,width=80,wrap=NONE)
-        self.w_log.grid(row=0,column=0)
-
-        self.sx_log = Scrollbar(self.f_log,orient=HORIZONTAL,command=self.w_log.xview)
+        self.sx_log = Scrollbar(self.root,orient=HORIZONTAL,command=self.w_log.xview)
         self.sx_log.grid(row=1,column=0,sticky=EW)
 
-        self.sy_log = Scrollbar(self.f_log,orient=VERTICAL,command=self.w_log.yview)
+        self.sy_log = Scrollbar(self.root,orient=VERTICAL,command=self.w_log.yview)
         self.sy_log.grid(row=0,column=1,sticky=NS)
 
         self.w_log.config(xscrollcommand=self.sx_log.set)

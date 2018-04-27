bc79e8215467e783e94e1ef4c6d2f90af823ce18
Venelin Kozhuharov <vkozhuharov@gmail.com>
2018-04-27 12:15:08 +0200

    Persistent TRecoVObject...

--- PadmeRoot/src/TRecoVObject.cc
+++ PadmeRoot/src/TRecoVObject.cc
@@ -5,9 +5,51 @@
 ClassImp(TRecoVObject)
 
 TRecoVObject::TRecoVObject(){
+    fNHits = 0;
+    fHits  = 0;
   ;
 }
 
 void TRecoVObject::Print(Option_t *) const {
   ;
+}
+TRecoVObject::~TRecoVObject(){
+  if (fHits) delete fHits;
+}
+
+
+void TRecoVObject::Clear(Option_t * option)
+{
+
+  fNHits = 0;
+  if(fHits) fHits->Clear(option);
+}
+
+TRecoVHit* TRecoVObject::AddHit()
+{
+  return (TRecoVHit*)(fHits->New(fNHits++));
+}
+
+TRecoVHit* TRecoVObject::AddHit(TRecoVHit* mcHit)
+{
+  TRecoVHit* hit = (TRecoVHit*)(fHits->New(fNHits++));
+  *hit = *mcHit;
+  return hit;
+}
+
+TRecoVHit* TRecoVObject::Hit(Int_t iHit)
+{
+  return (TRecoVHit*)fHits->At(iHit);
+}
+
+TRecoVHit* TRecoVObject::LastHit()
+{
+  return (TRecoVHit*)fHits->At(fNHits-1);
+}
+
+void TRecoVObject::RemoveHit(Int_t iHit)
+{
+    fHits->RemoveAt(iHit);
+    fHits->Compress();
+    fNHits--;
 }

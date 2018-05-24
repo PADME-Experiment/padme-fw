bd83c1127df5fdbf2047e43b0f4a876eed36a1b1
Venelin Kozhuharov <vkozhuharov@gmail.com>
2018-05-24 15:23:36 +0200

    HEPVeto initial reconstruction

--- PadmeReco/HEPVeto/include/HEPVetoReconstruction.hh
+++ PadmeReco/HEPVeto/include/HEPVetoReconstruction.hh
@@ -17,10 +17,10 @@ public:
   HEPVetoReconstruction(TFile*, TString);
   ~HEPVetoReconstruction();
 
-  void ParseConfFile(TString);
-  virtual void Init(PadmeVReconstruction*);
+  // void ParseConfFile(TString);
+  // virtual void Init(PadmeVReconstruction*);
   virtual void ProcessEvent(TMCVEvent*,TMCEvent*);
-  virtual void EndProcessing();
+  // virtual void EndProcessing();
 
 };
 #endif
--- PadmeReco/HEPVeto/src/HEPVetoReconstruction.cc
+++ PadmeReco/HEPVeto/src/HEPVetoReconstruction.cc
@@ -11,40 +11,42 @@
 #include "THEPVetoMCEvent.hh"
 #include "THEPVetoMCHit.hh"
 #include "THEPVetoMCDigi.hh"
+#include "DigitizerChannelReco.hh"
 
 HEPVetoReconstruction::HEPVetoReconstruction(TFile* HistoFile, TString ConfigFileName)
   : PadmeVReconstruction(HistoFile, "HEPVeto", ConfigFileName)
 {
   //fRecoEvent = new TRecoHEPVetoEvent();
   //ParseConfFile(ConfigFileName);
+  fChannelReco = new DigitizerChannelReco();
 }
 
 HEPVetoReconstruction::~HEPVetoReconstruction()
 {;}
 
-void HEPVetoReconstruction::Init(PadmeVReconstruction* MainReco)
-{
+// void HEPVetoReconstruction::Init(PadmeVReconstruction* MainReco)
+// {
 
-  //common part for all the subdetectors
-  PadmeVReconstruction::Init(MainReco);
+//   //common part for all the subdetectors
+//   PadmeVReconstruction::Init(MainReco);
 
-}
+// }
 
 // Read HEPVeto reconstruction parameters from a configuration file
-void HEPVetoReconstruction::ParseConfFile(TString ConfFileName) {
+// void HEPVetoReconstruction::ParseConfFile(TString ConfFileName) {
 
-  std::ifstream confFile(ConfFileName.Data());
-  if (!confFile.is_open()) {
-    perror(ConfFileName);
-    exit(1);
-  }
+//   std::ifstream confFile(ConfFileName.Data());
+//   if (!confFile.is_open()) {
+//     perror(ConfFileName);
+//     exit(1);
+//   }
 
-  TString Line;
-  while (Line.ReadLine(confFile)) {
-    if (Line.BeginsWith("#")) continue;
-  }
-  confFile.close();
-}
+//   TString Line;
+//   while (Line.ReadLine(confFile)) {
+//     if (Line.BeginsWith("#")) continue;
+//   }
+//   confFile.close();
+// }
 
 /*
 TRecoVEvent * HEPVetoReconstruction::ProcessEvent(TDetectorVEvent* tEvent, Event* tGenEvent)
@@ -72,5 +74,5 @@ void HEPVetoReconstruction::ProcessEvent(TMCVEvent* tEvent, TMCEvent* tMCEvent)
   }
 }
 
-void HEPVetoReconstruction::EndProcessing()
-{;}
+// void HEPVetoReconstruction::EndProcessing()
+// {;}

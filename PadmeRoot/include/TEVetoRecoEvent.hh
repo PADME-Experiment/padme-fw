e2cef63c0e15f004dfb14964d527ec84a6387541
Venelin Kozhuharov <vkozhuharov@gmail.com>
2018-05-24 15:16:16 +0200

    Persistent objects for the RECO hits of the individual detectors

---
+++ PadmeRoot/include/TEVetoRecoEvent.hh
@@ -0,0 +1,20 @@
+#ifndef TEVetoRecoEvent_H
+#define TEVetoRecoEvent_H
+
+#include "TRecoVObject.hh"
+
+class TEVetoRecoEvent : public TRecoVObject {
+
+public:
+
+  TEVetoRecoEvent(); 
+  ~TEVetoRecoEvent();
+
+private:
+  
+  
+
+
+  ClassDef(TEVetoRecoEvent,1);
+};
+#endif
---
+++ PadmeRoot/src/TTargetRecoEvent.cc
@@ -0,0 +1,10 @@
+
+#include "TTargetRecoEvent.hh"
+
+ClassImp(TTargetRecoEvent)
+
+TTargetRecoEvent::TTargetRecoEvent() : TRecoVObject(TRecoVHit::Class())
+{;}
+
+TTargetRecoEvent::~TTargetRecoEvent()
+{;}

a2573abad89ace8e1494a5e18d9e64f0228183fb
Venelin Kozhuharov <vkozhuharov@gmail.com>
2018-10-11 11:34:17 +0300

    Target configuration and monitoring

--- PadmeReco/Target/src/TargetReconstruction.cc
+++ PadmeReco/Target/src/TargetReconstruction.cc
@@ -11,19 +11,37 @@
 #include "TTargetMCEvent.hh"
 #include "TTargetMCHit.hh"
 #include "TTargetMCDigi.hh"
-#include "DigitizerChannelReco.hh"
+#include "DigitizerChannelTarget.hh"
+#include "TH2F.h"
+#include "TH1F.h"
+
 
 TargetReconstruction::TargetReconstruction(TFile* HistoFile, TString ConfigFileName)
   : PadmeVReconstruction(HistoFile, "Target", ConfigFileName)
 {
   //fRecoEvent = new TRecoTargetEvent();
   //ParseConfFile(ConfigFileName);
-  fChannelReco = new DigitizerChannelReco();
+  fChannelReco = new DigitizerChannelTarget();
 }
 
 void TargetReconstruction::HistoInit(){
-  AddHisto("TargetOccupancy",new TH1F("TargetOccupancy","Target Occupancy",33,0.0,33.0));
+  AddHisto("TargetProfile", new TH1F("TargetProfile" ,"Target profile"           ,32,0.5,32.5));
+  AddHisto("TargetProfileX",new TH1F("TargetProfileX","Target profile X"         ,16,-7.5,8.5));
+  AddHisto("TargetProfileY",new TH1F("TargetProfileY","Target profile Y"         ,16,-7.5,8.5));
+  AddHisto("TargetXYmap"   ,new TH2F("TargetXYmap"   ,"Target XY map",16,-7.5,8.5,16,-7.5,8.5));
+  
+  
+  GetHisto("TargetProfile")->GetXaxis()->SetTitle("StripX or StripY+16");
+  GetHisto("TargetProfile")->GetYaxis()->SetTitle("Charge[pC]");
+  
+  GetHisto("TargetProfileX")->GetXaxis()->SetTitle("x[mm]");
+  GetHisto("TargetProfileX")->GetYaxis()->SetTitle("Charge[pC]");
+   
+  GetHisto("TargetProfileY")->GetXaxis()->SetTitle("y[mm]");
+  GetHisto("TargetProfileY")->GetYaxis()->SetTitle("Charge[pC]");
 
+  ((TH2F *)GetHisto("TargetXYmap"))->GetXaxis()->SetTitle("x[mm]");
+  ((TH2F *)GetHisto("TargetXYmap"))->GetYaxis()->SetTitle("y[mm]");
 }
 
 
@@ -69,13 +87,40 @@ void TargetReconstruction::ProcessEvent(TMCVEvent* tEvent, TMCEvent* tMCEvent)
   }
 }
 
-// void TargetReconstruction::EndProcessing()
-// {;}
+//void TargetReconstruction::EndProcessing()
+//{;
+ //GetHisto("TargetOccupancy")->Fit("gaus","","",16.5,32.5);
+ //GetHisto("TargetOccupancy")->Fit("gaus","","",0.5, 16.5);
+//}
+
 void TargetReconstruction::AnalyzeEvent(TRawEvent* rawEv){
   vector<TRecoVHit *> &Hits  = GetRecoHits();
 
-  for(unsigned int iHit1 = 0; iHit1 < Hits.size();++iHit1) {
-    GetHisto("TargetOccupancy")->Fill(Hits[iHit1]->GetChannelId());
+  //std::cout<<" SS Target Hits.size "<< Hits.size()<<std::endl;
 
+  float Qx=0;
+  float Qy=0;
+  for(unsigned int iHit1 = 0; iHit1 < Hits.size();++iHit1) {
+    //std::cout<<" hit id, energy = "<<Hits[iHit1]->GetChannelId()<<" "<<Hits[iHit1]->GetEnergy()<<std::endl;
+    GetHisto("TargetProfile" )->Fill(Hits[iHit1]->GetChannelId()     ,Hits[iHit1]->GetEnergy());
+    if(iHit1<16){
+     Qx += Hits[iHit1]->GetEnergy();
+     GetHisto("TargetProfileX")->Fill(Hits[iHit1]->GetChannelId()   -8,Hits[iHit1]->GetEnergy());
+    }
+    else{
+     Qy += Hits[iHit1]->GetEnergy();
+     GetHisto("TargetProfileY")->Fill(Hits[iHit1]->GetChannelId()-16-8,Hits[iHit1]->GetEnergy());
+    }
   }
+
+  if(Hits.size()==32&&Qx>0&&Qy>0)for(unsigned int iHitx = 0; iHitx < 16;++iHitx) for(unsigned int iHity = 16; iHity < 32;++iHity){{
+    float qx = Hits[iHitx]->GetEnergy()/Qx; if(qx<0)qx= 0.01;
+    float qy = Hits[iHity]->GetEnergy()/Qy; if(qy>0)qy=-0.01;
+    float x  = float(Hits[iHitx]->GetChannelId())   -8;
+    float y  = float(Hits[iHity]->GetChannelId())-16-8;
+    ((TH2F *)GetHisto("TargetXYmap"))->Fill(x, y, -qx*qy);
+  }}
+
+
+
 }

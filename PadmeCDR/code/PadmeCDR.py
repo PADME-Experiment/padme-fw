a2f415a1d9c6808135a76f5064158332d18e3744
Emanuele Leonardi <emanuele.leonardi@roma1.infn.it>
2018-11-05 17:24:00 +0100

    PadmeCDR server now works on a single data server. User must start a copy for each data server

--- PadmeCDR/code/PadmeCDR.py
+++ PadmeCDR/code/PadmeCDR.py
@@ -8,9 +8,15 @@ import subprocess
 
 from PadmeCDRServer import PadmeCDRServer
 
+#def print_help():
+#    print 'PadmeCDR [-c config_file] [-i] [-h]'
+#    print '  -c config_file  Read CDR configuration from file <config_file>. (Currently not supported)'
+#    print '  -i              Run the PadmeCDR server in interactive mode'
+#    print '  -h              Show this help message and exit'
+
 def print_help():
-    print 'PadmeCDR [-c config_file] [-i] [-h]'
-    print '  -c config_file  Read CDR configuration from file <config_file>. (Currently not supported)'
+    print 'PadmeCDR -s data_server [-i] [-h]'
+    print '  -s              Define server from which data are copied'
     print '  -i              Run the PadmeCDR server in interactive mode'
     print '  -h              Show this help message and exit'
 
@@ -20,22 +26,38 @@ def main(argv):
     # Default to current dir if not set
     cdr_dir = os.getenv('PADME_CDR_DIR',".")
 
+    # Define list of available data servers
+    data_servers_list = [ "l1padme3", "l1padme4" ]
+
     try:
-        opts,args = getopt.getopt(argv,"ic:h")
+        #opts,args = getopt.getopt(argv,"ic:h")
+        opts,args = getopt.getopt(argv,"is:h")
     except getopt.GetoptError:
         print_help()
         sys.exit(2)
 
     cfg_file = ""
+    data_server = ""
     serverInteractive = False
     for opt,arg in opts:
         if opt == '-h':
             print_help()
             sys.exit()
-        elif opt == '-c':
-            cfg_file = arg
+        #elif opt == '-c':
+        #    cfg_file = arg
+        elif opt == '-s':
+            data_server = arg
         elif opt == '-i':
             serverInteractive = True
+
+    # Check if data server is valid
+    if (not data_server in data_servers_list):
+        if (data_server):
+            print "ERROR - Data server",data_server,"is unknown. Use one of",data_servers_list
+        else:
+            print "ERROR - You must specify one data server from",data_servers_list
+        print_help()
+        sys.exit(2)
 
     # Create long-lived proxy file (will ask user for password)
     long_proxy_file = "%s/run/long_proxy"%cdr_dir
@@ -47,10 +69,12 @@ def main(argv):
         sys.exit(2)
 
     if serverInteractive:
-        PadmeCDRServer(cfg_file,"i")
+        #PadmeCDRServer(cfg_file,"i")
+        PadmeCDRServer(data_server,"i")
     else:
         print "Starting PadmeCDRServer in background"
-        with daemon.DaemonContext(working_directory="."): PadmeCDRServer(cfg_file,"d")
+        #with daemon.DaemonContext(working_directory="."): PadmeCDRServer(cfg_file,"d")
+        with daemon.DaemonContext(working_directory="."): PadmeCDRServer(data_server,"d")
 
 # Execution starts here
 if __name__ == "__main__":

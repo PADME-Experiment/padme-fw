b02592fa5f108e0891e0bfd81083af69e4c87dd7
Venelin Kozhuharov <vkozhuharov@gmail.com>
2018-11-09 10:43:08 +0200

    Some updated Vetoes monitoring

--- PadmeReco/HEPVeto/src/HEPVetoReconstruction.cc
+++ PadmeReco/HEPVeto/src/HEPVetoReconstruction.cc
@@ -12,6 +12,8 @@
 #include "THEPVetoMCHit.hh"
 #include "THEPVetoMCDigi.hh"
 #include "DigitizerChannelReco.hh"
+#include "PadmeReconstruction.hh"
+#include "TH2F.h"
 
 HEPVetoReconstruction::HEPVetoReconstruction(TFile* HistoFile, TString ConfigFileName)
   : PadmeVReconstruction(HistoFile, "HEPVeto", ConfigFileName)
@@ -27,8 +29,9 @@ HEPVetoReconstruction::HEPVetoReconstruction(TFile* HistoFile, TString ConfigFil
 void HEPVetoReconstruction::HistoInit(){
   AddHisto("HEPVetoOccupancy",new TH1F("HEPVetoOccupancy","HEPVeto Occupancy",32,0.0,32.0));
   AddHisto("HEPVetoTime",new TH1F("HEPVetoTime","HEPVeto Time",1000,0.0,1000.0));
-
-  
+  AddHisto("HEPVetoMultiplicity",new TH1F("HEPVetoMultiplicity","Number of hits in the HEPVeto per event", 100, 0.0,100.0));
+  AddHisto("HEPVetoTimeVsPVetoTime",new TH2F("HEPVetoTimeVsPVetoTime","HEPVeto Time vs PVetoTime",100,100.0,500.0, 100,100.0,500.0));
+ 
 }
 
 
@@ -90,13 +93,40 @@ void HEPVetoReconstruction::ProcessEvent(TMCVEvent* tEvent, TMCEvent* tMCEvent)
 
 void HEPVetoReconstruction::AnalyzeEvent(TRawEvent* rawEv){
 
-
+  
   vector<TRecoVHit *> &Hits  = GetRecoHits();
   
+  UChar_t nBoards = rawEv->GetNADCBoards();
+  
+  TADCBoard* ADC;
+  
+  GetHisto("HEPVetoMultiplicity")->Fill(Hits.size());
   for(unsigned int iHit1 = 0; iHit1 < Hits.size();++iHit1) {
-    GetHisto("HEPVetoOccupancy")->Fill(Hits[iHit1]->GetChannelId());
+    if( Hits[iHit1]->GetTime() > 50.) {
+      //Real hit
+        GetHisto("HEPVetoOccupancy")->Fill(Hits[iHit1]->GetChannelId());
+    }
+    GetHisto("HEPVetoTime")->Fill(Hits[iHit1]->GetTime());
   }
 
+  PadmeReconstruction* MainReco = (PadmeReconstruction*  ) fMainReco;
   
+  
+  PadmeVReconstruction* PVetoReco = ( MainReco->FindReco("PVeto") );
+  
+  vector<TRecoVHit *> &PVetoHits  = PVetoReco->GetRecoHits();
+  
+  for(unsigned int iHit1 = 0; iHit1 < Hits.size();++iHit1) {
+    for(unsigned int iHit2 = 0; iHit2 < PVetoHits.size();++iHit2) {
+      if ( PVetoHits[iHit2]->GetChannelId() > 80 ) {
+          (  (TH2F *) GetHisto("HEPVetoTimeVsPVetoTime"))  ->Fill(Hits[iHit1]->GetTime(),PVetoHits[iHit2]->GetTime());
+      }
+      
+    }
+  }
+ 
+
+
+
 
 }

93a1d8dc58e3f34d57fce64aaa699fde6369964e
Emanuele Leonardi <emanuele.leonardi@roma1.infn.it>
2019-02-25 17:53:45 +0100

    PadmeCDR: fixed where VerifyRunsToRemove looks for VerifyRun

--- PadmeCDR/tools/VerifyRunsToRemove.sh
+++ PadmeCDR/tools/VerifyRunsToRemove.sh
@@ -6,6 +6,28 @@ usage() {
     exit 1
 }
 
+# Find where this script is really located: needed to find the corresponding VerifyRun.py script
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+VERIFYRUN=$DIR/VerifyRun.py
+if ! [[ -e $VERIFYRUN ]]; then
+    echo "ERROR - $VERIFYRUN does not exist"
+    usage
+fi
+if ! [[ -f $VERIFYRUN ]]; then
+    echo "ERROR - $VERIFYRUN is not a regular file"
+    usage
+fi
+if ! [[ -x $VERIFYRUN ]]; then
+    echo "ERROR - $VERIFYRUN is not executable"
+    usage
+fi
+
 # Define Storm access point to CNAF tape library and LNF storage system
 srm_cnaf="srm://storm-fe-archive.cr.cnaf.infn.it:8444/srm/managerv2?SFN=/padmeTape"
 srm_lnf="srm://atlasse.lnf.infn.it:8446/srm/managerv2?SFN=/dpm/lnf.infn.it/home/vo.padme.org"
@@ -42,6 +64,6 @@ fi
 # Get list of runs on DAQ servers for given year and verify each of them
 for srv in "${srv_list[@]}"; do
     for run in $( ssh -n -i $daq_keyfile -l $daq_user $srv ls ${daq_path}/${year}/rawdata ); do
-	./VerifyRun.py -R $run -S DAQ -s $srv -D $dst_site
+	$VERIFYRUN -R $run -S DAQ -s $srv -D $dst_site
     done
 done

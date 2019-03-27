156432c52e8ecaac36a7e43fbd328fce22e713eb
Stefania Spagnolo <spagnolo@wn-21.le.infn.it>
2019-03-27 15:30:56 +0100

    Fix algo2 for ECal clusterization

--- PadmeReco/ECal/src/ECalSimpleClusterization.cc
+++ PadmeReco/ECal/src/ECalSimpleClusterization.cc
@@ -13,7 +13,7 @@ Int_t ECalSimpleClusterization::IsSeedNeig(Int_t seedID, Int_t cellID) {
   Int_t CellCol=cellID%100;
   //excludes the seed cell 
   if( abs(SeedCol-CellCol)<=dChIDmax && abs(SeedRow-CellRow)<=dChIDmax ) 
-    IsNeig = (Int_t)std::min(abs(SeedCol-CellCol),abs(SeedRow-CellRow)); 
+    IsNeig = (Int_t)std::max(abs(SeedCol-CellCol),abs(SeedRow-CellRow)); 
   //  std::cout<<"seedID "<<seedID<<" cellID "<<cellID<<" Is Neig "<<IsNeig<<std::endl;
   return IsNeig;
 }

185a291f45820f47d35b68f89041499911027315
Stefania Spagnolo <spagnolo@wn-07.le.infn.it>
2019-06-12 12:15:14 +0200

    adding missing files for geometry & calibration

---
+++ PadmeReco/SAC/include/SACGeometry.hh
@@ -0,0 +1,34 @@
+// --------------------------------------------------------------
+// History:
+//
+// Created by Stefania Spagnolo (stefania.spagnolo@le.infn.it) 2019-03-14
+//
+// --------------------------------------------------------------
+#ifndef SACGeometry_H
+#define SACGeometry_H
+
+#include "PadmeVGeometry.hh"
+
+
+
+class SACGeometry : public PadmeVGeometry
+{
+
+public:
+  
+  SACGeometry();
+  //~SACGeometry();
+  void Init(PadmeVRecoConfig *cfg, RecoVChannelID *chIdMgr );
+  TVector3  LocalPosition(Int_t chId);
+  
+
+private:
+
+  double fCrystalGap;
+  double fCrystalCoating;
+  double fTedlarThickness;
+  
+ 
+};
+#endif
+

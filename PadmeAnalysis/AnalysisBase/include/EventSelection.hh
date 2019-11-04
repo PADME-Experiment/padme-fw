e2b15ddc10355e51c78700aa77e4f6a784cef338
Isabella Oceano <isabella@wn-10.le.infn.it>
2019-11-04 12:07:43 +0100

    EcalConfigure

--- PadmeAnalysis/AnalysisBase/include/EventSelection.hh
+++ PadmeAnalysis/AnalysisBase/include/EventSelection.hh
@@ -30,6 +30,57 @@ public:
   
 
   virtual Bool_t ProcessSelection();
+  
+  Bool_t phiSymmetricalInECal(TVector3 P1, TVector3 P2, double& distPhi);
+  
+  
+  void HistoInDeltaPhi3ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi3nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn1cmCoG3ns(Double_t g1E, Double_t g2E, Double_t InvariantMass,Double_t XWeighted,Double_t YWeighted,Double_t angleBetweenTwoPhoton,Double_t phig1,Double_t phig2,Double_t thetag1,Double_t thetag2,Double_t g1x,Double_t g2x,Double_t g1y,Double_t g2y, Double_t CoG);
+  void HistoIn1cmCoG3nsDeltaPhi(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn1cmCoG3nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass, Bool_t InDeltaPhi);
+  void HistoIn1cmCoG3nsInFiducialRegion(Double_t g1E, Double_t g2E, Double_t InvariantMass, Double_t CoG);
+  void HistoIn1cmCoG3nsInvariantMassRange(Double_t g1E, Double_t g2E,Double_t g1x, Double_t g2x, Double_t g1y, Double_t g2y, Double_t thetag1, Double_t thetag2, Double_t phig1, Double_t phig2);
+  void HistoIn2cmCoG3ns(Double_t g1E, Double_t g2E, Double_t InvariantMass, Double_t angleBetweenTwoPhoton);
+  void HistoIn2cmCoG3nsDeltaThetaDeltaPhi(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn2cmCoG3nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn3cmCoG3ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn3cmCoG3nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn3cmCoG3nsDeltaThetaDeltaPhi(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn5cmCoG3ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  
+  void HistoInDeltaPhi20DegreeSecondMethod3ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi10DegreeSecondMethod3ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi10DegreeSecondMethod3nsThrEnergy(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi5DegreeSecondMethod3ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi20DegreeSecondMethod3nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi10DegreeSecondMethod3nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi5DegreeSecondMethod3nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  
+  void HistoDeltaTheta_InDeltaPhi20Interval(Double_t angleBetweenTwoPhoton);
+  void HistoDeltaPhi_InDeltaThetaInterval(Double_t dphi);
+  
+  void HistoInDeltaPhi1_5ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi1_5nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn1cmCoG1_5ns(Double_t g1E, Double_t g2E, Double_t InvariantMass,Double_t XWeighted,Double_t YWeighted,Double_t angleBetweenTwoPhoton,Double_t phig1,Double_t phig2,Double_t thetag1,Double_t thetag2,Double_t g1x,Double_t g2x,Double_t g1y,Double_t g2y,Double_t CoG);
+  void HistoIn1cmCoG1_5nsDeltaPhi(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn1cmCoG1_5nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass, Bool_t InDeltaPhi);
+  void HistoIn1cmCoG1_5nsInFiducialRegion(Double_t g1E, Double_t g2E, Double_t InvariantMass, Double_t CoG);
+  void HistoIn1cmCoG1_5nsInvariantMassRange(Double_t g1E, Double_t g2E,Double_t g1x, Double_t g2x, Double_t g1y, Double_t g2y, Double_t thetag1, Double_t thetag2, Double_t phig1, Double_t phig2);
+  void HistoIn2cmCoG1_5ns(Double_t g1E, Double_t g2E, Double_t InvariantMass, Double_t angleBetweenTwoPhoton);
+  void HistoIn2cmCoG1_5nsDeltaThetaDeltaPhi(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn2cmCoG1_5nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn3cmCoG1_5ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn3cmCoG1_5nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn3cmCoG1_5nsDeltaThetaDeltaPhi(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoIn5cmCoG1_5ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  
+  void HistoInDeltaPhi20DegreeSecondMethod1_5ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi10DegreeSecondMethod1_5ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi5DegreeSecondMethod1_5ns(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi20DegreeSecondMethod1_5nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi10DegreeSecondMethod1_5nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
+  void HistoInDeltaPhi5DegreeSecondMethod1_5nsDeltaTheta(Double_t g1E, Double_t g2E, Double_t InvariantMass);
 
 
 protected:

54e1ceb1ae5be23966ca346a3ac2fb00776aa242
Stefania Spagnolo <spagnolo@wn-11.le.infn.it>
2019-11-15 13:16:22 +0100

    plotting macros

--- PadmeAnalysis/AnalysisTools/compare.C
+++ PadmeAnalysis/AnalysisTools/compare.C
@@ -1,10 +1,53 @@
-void compare(std::string hname, double scalef=-1, TFile* fData=_file0, TFile* fMC=_file1)
+#include <iostream>
+#include <cmath>
+
+#include "Rtypes.h"
+
+#include "PadmeUtils.h"
+#include "PadmeStyle.h"
+#include "PadmeLabels.h"
+
+/*
+#ifdef __CLING__
+// these are not headers - do not treat them as such - needed for ROOT6
+#include "PadmeLabels.C"
+#include "PadmeUtils.C"
+#endif
+*/
+
+#include "TCanvas.h"
+#include "TFile.h"
+#include "TROOT.h"
+#include "TH1F.h"
+#include "TRandom.h"
+#include "TGraphErrors.h"
+
+using namespace std;
+
+
+void compare(std::string hname, double scalef=-1, double xmin=-999, double xmax=999, 
+	       std::string xtitle="E(#gamma_{1})+E(#gamma_{2}) [MeV]", 
+	       TFile* fData=_file0, TFile* fMC=_file1)
 {
   //  TFile* fData = _file0;
   //  TFile* fMC   = _file1;
 
+  
+  /*    
+  #ifdef __CINT__
+    gROOT->LoadMacro("PadmeLabels.C");
+    gROOT->LoadMacro("PadmeUtils.C");
+  #endif
+
+  SetPadmeStyle();
+  */
+  
+
+  std::cout<<" pointer to data file and mc file ... "<<fData<<" "<<fMC<<"    processing histo "<<hname <<std::endl;
+  
   TH1D* hData = (TH1D*)fData->Get(hname.c_str());
   TH1D* hMC   = (TH1D*)fMC->Get(hname.c_str());
+
   
   TCanvas* cData = new TCanvas("data","data",500,500);
   hData->Draw();
@@ -40,21 +83,24 @@ void compare(std::string hname, double scalef=-1, TFile* fData=_file0, TFile* fM
   double ymax = ymaxMC;
   if (ymaxDT>ymax) ymax=ymaxDT;
   ymax = 1.3*ymax;
-  
+
   TH1D* hFrame = hMC->Clone();
   hFrame->Scale(0.);
   hFrame->SetMaximum(ymax);
 
+
   hFrame->Draw();
-  hsMC->SetLineColor(kBlack);
+  hsMC->SetLineColor(kRed);
   hsMC->Draw("SAME");
   hData->SetMarkerStyle(20);
-  hData->SetMarkerSize(0.5);
+  //hData->SetMarkerSize(0.5);
   hData->SetMarkerColor(kBlack);
   hData->Draw("SAMEPE");
 
   cout<<"Data/MC ratio = "<<hData->GetEntries()/hMC->GetEntries()<<std::endl;
   cout<<"Data/sMC ratio = "<<hData->GetEntries()/(scalef*hMC->GetEntries())<<std::endl;
+
+//return;
 
   TH1D* hratio = hMC->Clone();
   hratio->Scale(0.);
@@ -70,18 +116,20 @@ void compare(std::string hname, double scalef=-1, TFile* fData=_file0, TFile* fM
       }
       hratio->SetBinContent(i, rat); 
       hratio->SetBinError(i, erat); 
-      std::cout<<"Bin "<<i<<"  ratio = "<<hratio->GetBinContent(i)<<std::endl;
-      std::cout<<"Bin "<<i<<" eratio = "<<hratio->GetBinError(i)<<std::endl;
+      //std::cout<<"Bin "<<i<<"  ratio = "<<hratio->GetBinContent(i)<<std::endl;
+      //std::cout<<"Bin "<<i<<" eratio = "<<hratio->GetBinError(i)<<std::endl;
     }
   TCanvas* c2 = new TCanvas("ratio","ratio",600,300);
   hratio->Draw();
 
    // Define the Canvas
-   TCanvas *c = new TCanvas("c", "canvas", 800, 800);
+  TCanvas *c = new TCanvas("c", "My Final Plot", 50, 50, 600,600);
 
    // Upper plot will be in pad1
-   TPad *pad1 = new TPad("pad1", "pad1", 0, 0.3, 1, 1.0);
-   pad1->SetBottomMargin(0); // Upper and lower plot are joined
+   TPad *pad1 = new TPad("pad1", "pad1", 0, 0.3, 1, 1.);
+   pad1->SetBottomMargin(0.02); // Upper and lower plot are joined
+   pad1->SetTopMargin(1); // Upper and lower plot are joined
+   //pad1->SetLogy();
    //pad1->SetGridx();         // Vertical grid
    pad1->Draw();             // Draw the upper pad: pad1
    pad1->cd();               // pad1 becomes the current pad
@@ -91,12 +139,33 @@ void compare(std::string hname, double scalef=-1, TFile* fData=_file0, TFile* fM
    h1->Draw();               // Draw h1
    h2->Draw("same");         // Draw h2 on top of h1
    */
-   hFrame->SetStats(0);  
+
+   Double_t ymin=0.;
+   ymin = hMC->GetYaxis()->GetXmin();
+   if (pad1->GetLogy()) {
+     ymin = 1.;
+     ymax = 10*ymax;
+   }
+   
+   if (fabs(xmin+999)<0.0001 && fabs(xmax-999)<0.0001) {
+     xmin = hMC->GetXaxis()->GetXmin();
+     xmax = hMC->GetXaxis()->GetXmax();
+   }
+   //   Double_t xmin=60.00;  Double_t xmax=3500.;
+   TH1F *hFrame1 = pad1->DrawFrame(xmin,ymin,xmax,ymax);
+   std::cout<<"X range = "<<xmin<<" "<<xmax<<std::endl;
+   std::cout<<"Y range = "<<ymin<<" "<<ymax<<std::endl;
+   
+   hFrame1->GetXaxis()->SetLabelSize(0.);
+   hFrame1->SetStats(0);  
    hsMC->SetStats(0);  
    hData->SetStats(0);  
-   hFrame->Draw();
+   //hFrame1->Draw("SAME");
+   //return;
+
    hsMC->Draw("SAME");
    hData->Draw("SAMEPE");
+
 
 
    // Do not draw the Y axis label on the upper plot and redraw a small
@@ -109,9 +178,9 @@ void compare(std::string hname, double scalef=-1, TFile* fData=_file0, TFile* fM
 
    // lower plot will be in pad
    c->cd();          // Go back to the main canvas before defining pad2
-   TPad *pad2 = new TPad("pad2", "pad2", 0, 0.05, 1, 0.3);
-   pad2->SetTopMargin(0);
-   pad2->SetBottomMargin(0.2);
+   TPad *pad2 = new TPad("pad2", "pad2", 0, 0.0, 1, 0.3);
+   pad2->SetTopMargin(0.02);
+   pad2->SetBottomMargin(0.3);
    //pad2->SetGridx(); // vertical grid
    pad2->SetGridy(); // horizontal grid
    pad2->Draw();
@@ -129,14 +198,18 @@ void compare(std::string hname, double scalef=-1, TFile* fData=_file0, TFile* fM
    h3->SetMarkerStyle(21);
    h3->Draw("ep");       // Draw the ratio plot
    */
+   hratio->GetXaxis()->SetTitle("Y strip number");
    pad2->SetLogy();
+   TH1F *hFrameRat = pad2->DrawFrame(xmin,0.1,xmax,12.);
+   hFrameRat->GetXaxis()->SetTitle(xtitle.c_str());
+   hFrameRat->Draw();
    hratio->SetStats(0);  
-   hratio->SetMaximum(10.);
+   hratio->SetMaximum(12.);
    hratio->SetMinimum(0.1);
    //hratio->SetMinimum(0.1);
    hratio->SetMarkerStyle(20);
-   hratio->SetMarkerSize(0.5);
-   hratio->Draw("ep");
+   //hratio->SetMarkerSize(0.5);
+   hratio->Draw("epsame");
    
    /*
    // h1 settings
@@ -157,21 +230,60 @@ void compare(std::string hname, double scalef=-1, TFile* fData=_file0, TFile* fM
    //h3->SetTitle(""); // Remove the ratio title
    hratio->SetTitle("");
 
+   
    // Y axis ratio plot settings
-   hratio->GetYaxis()->SetTitle("Data/MC");
-   hratio->GetYaxis()->SetNdivisions(505);
-   hratio->GetYaxis()->SetTitleSize(20);
-   hratio->GetYaxis()->SetTitleFont(43);
-   hratio->GetYaxis()->SetTitleOffset(1.55);
-   hratio->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-   hratio->GetYaxis()->SetLabelSize(15);
+   hFrameRat->GetYaxis()->SetTitle("Data/MC");
+   hFrameRat->GetYaxis()->SetNdivisions(505);
+   hFrameRat->GetYaxis()->SetTitleSize(20);
+   hFrameRat->GetYaxis()->SetTitleFont(43);
+   hFrameRat->GetYaxis()->SetTitleOffset(1.55);
+   hFrameRat->GetYaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+   hFrameRat->GetYaxis()->SetLabelSize(20);
 
    // X axis ratio plot settings
-   hratio->GetXaxis()->SetTitleSize(20);
-   hratio->GetXaxis()->SetTitleFont(43);
-   hratio->GetXaxis()->SetTitleOffset(4.);
-   hratio->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
-   hratio->GetXaxis()->SetLabelSize(15);
+   hFrameRat->GetXaxis()->SetTitleSize(20);
+   hFrameRat->GetXaxis()->SetTitleFont(43);
+   hFrameRat->GetXaxis()->SetTitleOffset(3.5);
+   hFrameRat->GetXaxis()->SetLabelFont(43); // Absolute font size in pixel (precision 3)
+   hFrameRat->GetXaxis()->SetLabelSize(20);
+   
 
+   pad1->cd();
+   PADME_LABEL(0.2,0.8); myText( 0.33,0.8,1,"Internal");
+  //   ATLASLabel(0.2,0.8,"Work in progress");//,"Preliminary");
+   myText(       0.2, 0.7 , 1, "nPOT=9.3#times10^{9}");
 
+   // myText(       0.63, 0.7 , 1, "Diamond Target");
+   // myText(       0.57, 0.8, 1, "Average beam Y profile");
+
+   //myMarkerText( 0.25, 0.5, 1, 20, "Data July 2019",1.3);
+   //myBoxText(    0.25, 0.45, 0.05, kBlack, "MC");
+   TLegend* t = new TLegend(0.55,0.7,0.80,0.85);
+   t->AddEntry(hData,"data July 2019","ep");
+   t->AddEntry(hsMC,"MC 20k e^{+}/bunch on target","l");
+   t->SetTextAlign(12);
+   t->SetBorderSize(0);
+   t->SetFillStyle(0);
+   t->SetFillColor(0);
+   t->SetTextFont(43);
+   t->SetTextSize(15);
+
+   t->Draw();
+
+   
+   c->SaveAs(("outpng/"+hname+".png").c_str());
+   //c->SaveAs("myPlot.png");
+   //c->SaveAs("myPlot.eps");
+   //return;
+
+   
+   delete pad1;
+   delete pad2;
+   delete cData;
+   delete cMC;
+   delete c;
+   delete c2;
+   delete t;
+   
+   std::cout<<"Bye, bye ................."<<std::endl<<std::endl;
 }
---
+++ PadmeAnalysis/AnalysisTools/produceSelectionPlots.C
@@ -0,0 +1,153 @@
+#include "PadmeUtils.h"
+#include "PadmeStyle.h"
+#include "PadmeLabels.h"
+
+#include "compare.C"
+
+
+using namespace std;
+
+void produceSelectionPlots(TFile* fData=_file0, TFile* fMC=_file1)
+{
+  
+  #ifdef __CINT__
+    gROOT->LoadMacro("PadmeLabels.C");
+    gROOT->LoadMacro("PadmeUtils.C");
+    gROOT->LoadMacro("PadmeStyle.C");
+  #endif
+
+  SetPadmeStyle();
+
+
+  std::string xtitle;
+  double xmin=-999;
+  double xmax= 999;
+
+
+  xtitle = "Number of ECal hits";
+  xmin = 0.;
+  xmax = 400.;
+  compare("SS2g_nhits",                    -1, xmin, xmax, xtitle, fData, fMC);
+  xtitle = "Cluster size";
+  xmin = -999;
+  xmax =  999;
+  compare("SS2g_clSize",                    -1, xmin, xmax, xtitle, fData, fMC);
+
+  //return;
+
+  xtitle = "E(#gamma_{1}+E(#gamma_{2}) [MeV]";
+  xmin = 0.;
+  xmax = 1200.;
+  compare("SS2gSumE_passDt",                    -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gSumE_passDtDphi",                -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gSumE_passDtDphiCog",             -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gSumE_passDtDphiCogFR",           -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gSumE_passDtDphiCogDsume",        -1, 400, 600, xtitle, fData, fMC);
+  //compare("SS2gSumE_passDtDphiCogDsume",        0.38, 400., 600, xtitle, fData, fMC);
+  //compare("SS2gSumE_passDtDphiCogDsume",        0.2144, 400., 600, xtitle, fData, fMC);
+
+
+
+  xtitle = "#Deltat [ns]";
+  xmin = -15.;
+  xmax =  15.;
+  compare("SS2gDt_passDtDphi",                -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gDt_passDtDphiCog",             -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gDt_passDtDphiCogFR",           -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gDt_passDtDphiCogDsume",        -1, xmin, xmax, xtitle, fData, fMC);
+
+  //  return;
+
+  xmin = -150.;
+  xmax =  150.;
+  xtitle = "#Deltat (hit-hit, ECal) [ns]";
+  compare("TimeSpreadInECal",                 -1, xmin, xmax, xtitle, fData, fMC);
+  xmin = -15.;
+  xmax =  15.;
+  xtitle = "#Deltat (in ECal clusters) [ns]";
+  compare("TimeSpreadInECalClus",             -1, xmin, xmax, xtitle, fData, fMC);
+
+  xmin = -100.;
+  xmax =  400.;
+  xtitle = "T(#gamma_{1}) (E>50MeV)[ns]";
+  compare("SS2g_ClTime",                      -1, xmin, xmax, xtitle, fData, fMC);
+  xtitle = "T(#gamma_{1}) (#gamma#gamma candidate)[ns]";
+  compare("SS2g_ClTime_passDtDphiCogDsume",   -1, xmin, xmax, xtitle, fData, fMC);
+
+  xmin=50.;
+  xmax=500.;
+  xtitle = "E(#gamma) [MeV]";
+  compare("SS2gE_passDtDphiCogDsume",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gE_passDtDphiCogFR",      -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gE_passDtDphiCog",        -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gE_passDtDphi",           -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gE_passDt",               -1, xmin, xmax, xtitle, fData, fMC);
+
+  xtitle = "#Delta#phi [deg]";
+  xmax=180.2;
+  compare("SS2gDphi_passDt",   -1, xmin, xmax, xtitle, fData, fMC);
+  xmin=129.8;
+  compare("SS2gDphi_passDtDphi",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gDphi_passDtDphiCog",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gDphi_passDtDphiCogFR",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gDphi_passDtDphiCogDsume",   -1, xmin, xmax, xtitle, fData, fMC);
+
+  xmin = -250.;
+  xmax =  250.;
+  xtitle = "X centroid [mm]";
+  compare("SS2gXcog_passDt",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gXcog_passDtDphi",   -1, xmin, xmax, xtitle, fData, fMC);
+  xmin = -40.;
+  xmax =  40.;
+  compare("SS2gXcog_passDtDphiCogFR",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gXcog_passDtDphiCogDsume",   -1, xmin, xmax, xtitle, fData, fMC);
+
+
+  xmin = -250.;
+  xmax =  250.;
+  xtitle = "Y centroid [mm]";
+  compare("SS2gYcog_passDt",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gYcog_passDtDphi",   -1, xmin, xmax, xtitle, fData, fMC);
+  xmin = -40.;
+  xmax =  40.;
+  compare("SS2gYcog_passDtDphiCogFR",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gYcog_passDtDphiCogDsume",   -1, xmin, xmax, xtitle, fData, fMC);
+
+
+  xmin = -350.;
+  xmax =  350.;
+  xtitle = "X [mm]";
+  compare("SS2gX_passDt",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gX_passDtDphi",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gX_passDtDphiCog",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gX_passDtDphiCogFR",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gX_passDtDphiCogDsume",   -1, xmin, xmax, xtitle, fData, fMC);
+
+
+  xtitle = "Y [mm]";
+  compare("SS2gY_passDt",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gY_passDtDphi",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gY_passDtDphiCog",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gY_passDtDphiCogFR",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gY_passDtDphiCogDsume",   -1, xmin, xmax, xtitle, fData, fMC);
+
+  xmin = -350.;
+  xmax =  350.;
+  xtitle = "X (energy weigthed) [mm]";
+  compare("SS2gXEw_passDt",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gXEw_passDtDphi",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gXEw_passDtDphiCog",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gXEw_passDtDphiCogFR",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gXEw_passDtDphiCogDsume",   -1, xmin, xmax, xtitle, fData, fMC);
+
+
+  xtitle = "Y (energy weigthed) [mm]";
+  compare("SS2gYEw_passDt",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gYEw_passDtDphi",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gYEw_passDtDphiCog",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gYEw_passDtDphiCogFR",   -1, xmin, xmax, xtitle, fData, fMC);
+  compare("SS2gYEw_passDtDphiCogDsume",   -1, xmin, xmax, xtitle, fData, fMC);
+
+
+}
+

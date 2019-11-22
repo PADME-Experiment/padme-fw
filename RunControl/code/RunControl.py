8779384423833e9d863d88fa158339d5bdd7d93c
Emanuele Leonardi <emanuele.leonardi@roma1.infn.it>
2019-11-22 12:08:54 +0100

    RunControl: changed default to not use GUI

--- RunControl/code/RunControl.py
+++ RunControl/code/RunControl.py
@@ -14,16 +14,18 @@ def main(argv):
     try:
         opts,args = getopt.getopt(argv,"h",["no-gui","server","interactive"])
     except getopt.GetoptError:
-        print 'RunControl [--no-gui] [--server] [--interactive] [-h|--help]'
+        print 'RunControl [--gui|--no-gui] [--server] [--interactive] [-h|--help]'
         sys.exit(2)
 
-    useGUI = True
+    useGUI = False
     startServer = False
     serverInteractive = False
     for opt,arg in opts:
         if opt == '-h' or opt == '--help':
-            print 'RunControl [--no-gui] [--server] [--interactive] [-h]'
+            print 'RunControl [--gui|--no-gui] [--server] [--interactive] [-h]'
             sys.exit()
+        elif opt == '--gui':
+            useGUI = True
         elif opt == '--no-gui':
             useGUI = False
         elif opt == '--server':
@@ -41,7 +43,8 @@ def main(argv):
 
     elif useGUI:
 
-        app = RunControlGUI()
+        print "Sorry: RunControl GUI is currently not available. Please use --no-gui option to start the text version."
+        //app = RunControlGUI()
 
     else:
 

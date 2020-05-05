391bbb4a5f75cafbaa3c4ac9cabb5301349cdd98
Emanuele Leonardi <emanuele.leonardi@roma1.infn.it>
2020-05-05 10:35:02 +0200

    PadmeRoot: added 64bit Status word to TRecoVHit and TRecoVCluster

--- PadmeRoot/src/TRecoVCluster.cc
+++ PadmeRoot/src/TRecoVCluster.cc
@@ -6,17 +6,7 @@ ClassImp(TRecoVCluster)
 
 TRecoVCluster::TRecoVCluster():TMCVHit()
 {
-  // // Initialize to invalid channel
-  // fChannelId = -1;
-  // fEnergy    = 0.;
-  // fTime      = 1.E9;
+  fSeed = -1;
+  fNHitsInClus = 0;
+  fStatus = 0;
 }
-
-
-// void TRecoVCluster::Print(Option_t *) const {
-//   std::cout << "Hit - ChannelId = " << fChannelId
-// 	    << " HitPosition = (" << fPosition.X() << "," << fPosition.Y() << "," << fPosition.Z() << ")"
-// 	    << " Energy = " << fEnergy
-// 	    << " Time = " << fTime << std::endl;
-// }
-

792f9a9ec391fb883c4539b8dce021e5780df99e
BethLong06 <48765589+BethLong06@users.noreply.github.com>
2022-11-03 15:17:52 +0100

    Updates for Mac

---
+++ PadmeRoot/include/TETagRecoEvent.hh
@@ -0,0 +1,17 @@
+#ifndef TETagRecoEvent_H
+#define TETagRecoEvent_H
+
+#include "TRecoVObject.hh"
+
+class TETagRecoEvent : public TRecoVObject {
+
+public:
+
+  TETagRecoEvent(); 
+  ~TETagRecoEvent();
+
+private:
+
+  ClassDef(TETagRecoEvent,1);
+};
+#endif

56fc64835db34f3a22a81abec9b9950be86d72b1
BethLong06 <48765589+BethLong06@users.noreply.github.com>
2023-02-08 12:16:57 +0100

    fixed bug in PVetoReconstruction clustering for data, making sure that cluster time is the average time

--- PadmeReco/PVeto/src/PVetoReconstruction.cc
+++ PadmeReco/PVeto/src/PVetoReconstruction.cc
@@ -149,7 +149,7 @@ void PVetoReconstruction::ConvertMCDigitsToRecoHits(TMCVEvent* tEvent,TMCEvent* 
       //smearing in time
       //      Hit->SetTime(smearedtime);
       Hit->SetTime(Hit->GetTime()/Hit->GetEnergy());
-      Noise=random->Gaus(0.,fSigmaNoiseForMC);   
+      //      Noise=random->Gaus(0.,fSigmaNoiseForMC);   
       Hit->SetEnergy(fMCEnergyScale*(Hit->GetEnergy()+Noise));
       if(Hit->GetEnergy()>fMCEnergyThr) fHits.push_back(Hit);
       else std::cout<<"Dumping "<<Hit->GetEnergy()<<" MeV hit"<<std::endl;

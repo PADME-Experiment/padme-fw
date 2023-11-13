14f2fdd56581b807a68326bef6d4ea1cc595b939
mancinima <mancinima@lnf.infn.it>
2023-11-13 16:41:48 +0100

    hoping I am not wrong - marco branch fortunately

--- PadmeReco/HEPVeto/include/HEPVetoSimpleClusterization.hh
+++
@@ -1,9 +0,0 @@
-#ifndef HEPVETOSIMPLE_CLUSTERIZATION_H
-#define HEPVETOSIMPLE_CLUSTERIZATION_H
-#include "PadmeVClusterization.hh"
-
-
-class HEPVetoSimpleClusterization : public PadmeVClusterization{
-};
-#endif
-
--- PadmeReco/PVeto/include/PVetoSimpleClusterization.hh
+++
@@ -1,9 +0,0 @@
-#ifndef PVETOSIMPLE_CLUSTERIZATION_H
-#define PVETOSIMPLE_CLUSTERIZATION_H
-#include "PadmeVClusterization.hh"
-
-
-class PVetoSimpleClusterization : public PadmeVClusterization{
-};
-#endif
-

1f02ae397636761082d7b7fa105254d21a676fa9
Emanuele Leonardi <emanuele.leonardi@roma1.infn.it>
2025-05-23 08:05:28 +0200

    OnlineMonitor: added watchdog to SmallChamberMonitor

---
+++ OnlineMonitor/SmallChamberMonitor_wd.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Pause between checks in seconds
+pause=10
+
+# Current year
+year=$( date +%Y )
+
+# Rawdata directory
+inputDir="/mnt/l0padme2/MMchamber"
+
+# Main configuration file
+configFile="config/OnlineMonitor.cfg"
+
+# Watchdir directory (used by PadmeMonitor)
+watchDir="/home/monitor/PadmeMonitor/watchdir"
+
+# File with name of current run
+#current_run_file="/home/daq/DAQ/run/current_run"
+
+# File with name of last finished run
+#last_run_file="/home/daq/DAQ/run/last_run"
+
+# Variable to save last run started
+current_run_save=""
+
+# Initialize status of SmallChamberMonitor process to NOT RUNNING
+om_running=0
+
+while true; do 
+
+    # Get current run
+    current_run=$( ls -rt $inputDir | grep .root | tail -1 | sed -e "s/.root//" )
+
+    # Check if the run has changed
+    if [[ $current_run != $current_run_save ]]; then
+
+	# If old SmallChamberMonitor process status is RUNNING, tell it to stop and change the status to NOT RUNNING
+	if [ "$om_running" -eq "1" ]; then
+	    touch $stopFile
+	    om_running=0
+	fi
+
+	# Log and error files for SmallChamberMonitor of new run
+	logFile="log/SMM_${current_run}.log"
+	errFile="log/SMM_${current_run}.err"
+
+	# File used to stop this SmallChamberMonitor
+	stopFile="run/SMM_${current_run}.stop"
+
+	# Add a pause to allow file to really appear (NFS is tricky)
+	sleep $pause
+
+	now=$( date -u )
+	echo
+	echo "*** $now - Starting SmallChamberMonitor ***"
+	echo "  Run: $current_run"
+	echo "  Input rawdata directory: $inputDir"
+
+	# Start ChamberMonitor for new run
+	echo "> stdbuf -oL nohup ./SmallChamberMonitor -f -r -R $current_run -D $inputDir -c $configFile -o $watchDir -s $stopFile >>$logFile 2>$errFile </dev/zero &"
+	stdbuf -oL nohup ./SmallChamberMonitor -f -r -R $current_run -D $inputDir -c $configFile -o $watchDir -s $stopFile >>$logFile 2>$errFile </dev/zero &
+	om_pid=$!
+
+	# Change status of ChamberMontior process to RUNNING
+	om_running=1
+
+	# Save name of current run
+	current_run_save=$current_run
+
+    fi
+
+    # Check if SmallChamberMonitor process is still running and restart it if it is dead
+    if [ "$om_running" -eq "1" ]; then
+	kill -s 0 $om_pid 2>/dev/null
+	if [ $? -ne 0 ]; then
+	    now=$( date -u )
+	    echo "$now - WARNING - SmallChamberMonitor process $om_pid is dead but run $current_run is still active: restart it"
+	    echo "> stdbuf -oL nohup ./SmallChamberMonitor -f -r -R $current_run -D $inputDir -c $configFile -o $watchDir -s $stopFile >>$logFile 2>$errFile </dev/zero &"
+	    stdbuf -oL nohup ./SmallChamberMonitor -f -r -R $current_run -D $inputDir -c $configFile -o $watchDir -s $stopFile >>$logFile 2>$errFile </dev/zero &
+	    om_pid=$!
+	    sleep 60
+	fi
+    fi
+
+    sleep $pause
+
+done
